22276c8108e615764137c1d5fae77513f192a904
Rufat Khaslarov <rufat.khaslarov@gmail.com>
2018-03-04 22:34:58 +0300

    bubble fix

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Awesome Interview Questions
   - [Big O Notation](https://www.youtube.com/watch?v=v4cd1O4zkGw)
   - [Big O Cheatsheet](http://bigocheatsheet.com/)
+  - [Bubble Sort](https://www.youtube.com/watch?v=6Gv8vg0kcHc)
--- bubble_sort.js
+++ bubble_sort.js
@@ -8,20 +8,29 @@ function bubbleSort(array) {
 
     const result = [...array];
 
+    let isSorted = false;
+
     for (let padding = 0; padding < result.length; padding++) {
+
+        isSorted = true;
 
         for (let counter = 1; counter < result.length - padding; counter++) {
 
             const prev = result[counter - 1];
             const next = result[counter];
 
-            if (prev > next)
+            if (prev > next) {
+                isSorted = false;
                 swap(result, counter - 1, counter);
+            }
         }
+
+        if (isSorted)
+            break;
     }
 
     return result;
 }
 
 
-console.log(bubbleSort([8, 1, 3, 4, 5, 3, 7, 9]));
+console.log(bubbleSort([8, 1, 3, 10, 4, 5, 3, 7, 9]));
