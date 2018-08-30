a284e46b57c01282503f8d7bb8cee3ff92c84f26
Rufat Khaslarov <rufat.khaslarov@gmail.com>
2018-08-30 21:26:36 +0300

    chunk challenge

---
+++ challenges/chunk/index.js
@@ -0,0 +1,40 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
+
+function chunk(array, size) {
+    return array.reduce((chunked, current) => {
+
+        const last = chunked[chunked.length - 1];
+
+        if (!last || last.length === size) {
+            chunked.push([current]);
+        } else {
+            last.push(current);
+        }
+
+        return chunked;
+
+    }, []);
+}
+
+console.log(chunk([1, 2, 3, 4], 2));
