d063e31f4f1af82a95ffd56119b2d7d80e8e1894
Rufat Khaslarov <rufat.khaslarov@gmail.com>
2019-01-20 20:10:04 +0300

    quicksort simple version

--- sorting-algorithms/bubble_sort.js
+++ sorting-algorithms/bubble_sort.js
@@ -1,16 +1,18 @@
-// This is often the easiest to conceptualize and a natural way
-// for the brain to think about sorting so it's typical to do bubble sort first.
-// It's also amongst the least efficient in terms of worst case scenario.
-//
-// In bubble sort, we're going to loop through the array and compare each index with the index next to it.
-// If the those two numbers are out of order (the lesser index's value is greater than the greater index's value)
-// we swap those two numbers' places in the array. We keep looping over that array until everything is
-// in place and nothing was swapped during the last iteration.
-//
-// What's the Big O on this? Well, there's an inner loop to check to see if indexes need to be swapped,
-// and an outer loop that's just checking to see if anything was swapped.
-// That would be make it O(n²). Not efficient, but a great learning tool.
-// You'll never use bubble sort for anything serious.
+/*
+    This is often the easiest to conceptualize and a natural way
+    for the brain to think about sorting so it's typical to do bubble sort first.
+    It's also amongst the least efficient in terms of worst case scenario.
+
+    In bubble sort, we're going to loop through the array and compare each index with the index next to it.
+    If the those two numbers are out of order (the lesser index's value is greater than the greater index's value)
+    we swap those two numbers' places in the array. We keep looping over that array until everything is
+    in place and nothing was swapped during the last iteration.
+
+    What's the Big O on this? Well, there's an inner loop to check to see if indexes need to be swapped,
+    and an outer loop that's just checking to see if anything was swapped.
+    That would be make it O(n²). Not efficient, but a great learning tool.
+    You'll never use bubble sort for anything serious.
+*/
 
 function swap(array, prev, next) {
     const temp = array[prev];
--- sorting-algorithms/quick_sort.js
+++ sorting-algorithms/quick_sort.js
@@ -1,3 +1,34 @@
+/*
+
+  Quicksort!
+
+  Quicksort should grab a pivot from the end and then separate the list (not including the pivot)
+  into two lists, smaller than the pivot and larger than the pivot. Call quickSort on both of those
+  lists independently. Once those two lists come back sorted, concatenate the "left" (or smaller numbers)
+  list, the pivot, and the "right" (or larger numbers) list and return that. The base case is when quickSort
+  is called on a list with length less-than-or-equal-to 1. In the base case, just return the array given.
+*/
+
+function quickSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const pivot = array[array.length - 1];
+    const left = [];
+    const right = [];
+
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] < pivot) {
+            left.push(array[i]);
+        } else {
+            right.push(array[i]);
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
 function swap(array, prev, next) {
     const temp = array[prev];
     array[prev] = array[next];
@@ -26,22 +57,23 @@ function partition(array, left, right) {
     return left;
 }
 
-function quickSort(array, left, right) {
+function quickSort2(array, left, right) {
     if (left < right && array.length) {
         const index = partition(array, left, right);
 
         if (left < index - 1) {
-            quickSort(array, left, index - 1);
+            quickSort2(array, left, index - 1);
         }
 
         if (index < right) {
-            quickSort(array, index, right);
+            quickSort2(array, index, right);
         }
     }
 }
 
 const array = [8, 1, 3, 10, 4, 5, 3, 7, 9];
 
-quickSort(array, 0, array.length - 1);
+quickSort2(array, 0, array.length - 1);
 
 console.log(array);
+console.log(quickSort([8, 1, 3, 10, 4, 5, 3, 7, 9]));
