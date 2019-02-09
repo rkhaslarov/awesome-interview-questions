1e411710aef6b82d5a794f0b6cd66e8a4d898152
Rufat Khaslarov <rufat.khaslarov@gmail.com>
2019-02-09 13:43:15 +0300

    eslint fixes

--- challenges/twosum/index.js
+++ challenges/twosum/index.js
@@ -5,11 +5,9 @@
 // twoSum([2, 7, 11, 15], 9) === [0, 1]
 
 function twoSum(nums, target) {
-
     const map = {};
 
     for (let i = 0; i < nums.length; i++) {
-
         const current = nums[i];
         const complement = target - current;
 
@@ -19,4 +17,8 @@ function twoSum(nums, target) {
 
         map[complement] = i;
     }
-};
+
+    return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
